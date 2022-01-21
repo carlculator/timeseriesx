8d9c4a5842e9a0fb24f25fdd63ac975c35230c64
Alexander Schulz <info@alexander-schulz.eu>
2022-01-21 14:42:22 +0100

    Bump version: 0.1.9 → 0.1.10

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/carlculator/timeseriesx',
-    version='0.1.9',
+    version='0.1.10',
     zip_safe=False,
 )
